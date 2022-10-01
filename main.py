28dd6a385c1380dd5a8b2989c4e3ad1a2c0d6184
Deos <newagearchangel@gmail.com>
2022-10-01 10:17:39 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import base64
-path = r'path-to-file' # need to replace with function parameters
-with open(path,'r') as f:
-    x= base64.b64encode(f.read())
 
-print(x)
+def convert_img(path_to_img_file):
+    with open(path_to_img_file,'r') as f:
+        x= base64.b64encode(f.read())
+    return x
+
+path_to_file = r'path-to-file' # need to replace with function parameters
+
