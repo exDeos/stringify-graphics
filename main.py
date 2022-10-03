74e10131ea33537f13aabf5af1e52e2aa2562c5a
Deos <newagearchangel@gmail.com>
2022-10-03 17:13:20 -0400

    Update main.py
    added a PIL Image creation utility

--- main.py
+++ main.py
@@ -8,5 +8,11 @@ def convert_img(path_to_img_file):
 path_to_file = r'path-to-file' # need to replace with function parameters
 
 from PIL import Image
+from io import BytesIO
+
+def make_img(imstr):
+    with BytesIO(base64.b64decode(imstr) as f:
+        im= Image.open(f)
+    return im
 
 # to be added later ?
